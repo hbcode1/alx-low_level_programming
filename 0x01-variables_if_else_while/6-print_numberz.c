718e98e159f75c2d87e2b4b84b9d4b999424c9a5
houcine <houcine.bh@pm.me>
2023-02-17 10:38:14 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+ * This program prints all single digit numbers of base 10
+ * starting from 0,
+ * followed by a new line.
+ *
+ */
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* declare n */
+	int n;
+	/* init n form 48 (dec of 0 in ascii) */
+	n = 48;
+	/* print numbers */
+	while (n < 58)
+	{
+		putchar(n);
+		/* increment n */
+		n++;
+	}
+	/* print new line */
+	putchar(10);
+	return (0);
+}
