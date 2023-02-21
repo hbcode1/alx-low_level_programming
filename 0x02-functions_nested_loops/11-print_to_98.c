c6df8c5766c63dc0d73f549ceb0b929c99251f3b
houcine <houcine.bh@pm.me>
2023-02-21 15:36:20 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < 98)
+	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
@@ -24,7 +24,7 @@ void print_to_98(int n)
 			}
 		}
 	}
-	if (n > 98)
+	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
