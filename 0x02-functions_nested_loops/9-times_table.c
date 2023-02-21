48fb50b1b357fc6be6bc46c4b036c2868c3f3b77
houcine <houcine.bh@pm.me>
2023-02-21 14:25:59 -0800

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,14 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, c;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			write(1, &(i * j), sizeof(i * j));
+			c = i * j;
+			write(1, &c, sizeof(c));
 		}
 		_putchar(10);
 
