12e638c2f94a0ed96aa0cbce89794c0683e98fe3
houcine <houcine.bh@pm.me>
2023-02-21 15:08:45 -0800

    8. Jack Bauer time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59.
- * Return: value of the last digit
+ * Return: value of the last digit.
  */
 void jack_bauer(void)
 {
