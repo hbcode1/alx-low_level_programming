5697dcba717faf66e843e7adcb5bfbc08c9f4ba9
houcine <houcine.bh@pm.me>
2023-02-21 13:35:34 -0800

    0

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,14 +6,13 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int i, j, k, l;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 60; j++)
+		for (j = 0; j < 10; j++)
 		{
 			_putchar(i + '0');
-			_putchar(58);
 			_putchar(j + '0');
 			_putchar(10);
 		}
