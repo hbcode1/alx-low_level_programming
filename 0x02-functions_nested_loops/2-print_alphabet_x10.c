761759df665df238857ba4c0ce20c62618090084
houcine <houcine.bh@pm.me>
2023-02-21 08:00:52 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: void.
+ */
+void print_alphabet(void)
+{
+	int i;
+	int j;
+	
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar(10);
+	}
+}
+
