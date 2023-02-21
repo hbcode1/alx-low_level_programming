b9fef1aa5d7509caaf0fc762f3a4fc0609b28cf4
houcine <houcine.bh@pm.me>
2023-02-21 13:16:02 -0800

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ * Return: value of the last digit
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar('0' + i);
+			_putchar(58);
+			_putchar('0' + j);
+		}
+	}
+}
