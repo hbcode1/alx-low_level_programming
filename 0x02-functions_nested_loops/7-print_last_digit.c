b83a99eb32e919d0dc26036d8538bd2e77920416
houcine <houcine.bh@pm.me>
2023-02-21 11:37:16 -0800

    7. prints the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @num: number to check
+ * Return: value of the last digit
+ */
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
