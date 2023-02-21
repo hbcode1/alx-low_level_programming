b9d93a2d1c6acd82731c1c538aacc1da95047be8
houcine <houcine.bh@pm.me>
2023-02-21 10:45:20 -0800

    6. absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: absolute int
+ * @int: integer to be computed
+ */
+int _abs(int)
+{
+	return (int *int);
+}
