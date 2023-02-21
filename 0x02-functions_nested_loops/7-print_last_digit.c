9457d7a24d47a3866a83a48719f50fe820947f26
houcine <houcine.bh@pm.me>
2023-02-21 15:07:28 -0800

    7. the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,8 +2,8 @@
 #include "6-abs.c"
 /**
  * print_last_digit - prints the last digit of a number.
- * @num: number to check
- * Return: value of the last digit
+ * @num: number to check.
+ * Return: value of the last digit.
  */
 int print_last_digit(int num)
 {
