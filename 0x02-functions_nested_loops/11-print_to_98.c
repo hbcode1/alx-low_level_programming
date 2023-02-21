244ba94d260c5490cea7be2acb9cf89e7037f6ae
houcine <houcine.bh@pm.me>
2023-02-21 15:59:34 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,11 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i !== 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 	else if (n > 98)
@@ -24,6 +28,10 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d, ", i);
+			if (i !== 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 	printf("\n");
