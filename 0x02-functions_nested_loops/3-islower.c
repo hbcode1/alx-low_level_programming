df33bf78fa3f47712e173595457064fc13e19998
houcine <houcine.bh@pm.me>
2023-02-21 08:26:19 -0800

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - Function checks for lowercase characters.
+
+ * Return: 1 (True), 0 (False).
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c > 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+	
+
