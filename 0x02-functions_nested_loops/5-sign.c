da0c9d1dc79dc76e04ca5bc781c9100ac403b9cd
houcine <houcine.bh@pm.me>
2023-02-21 09:50:10 -0800

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * print_sign - prints the sign of a number
  *
  * Return: 1 positive, 0 null, -1 negative
@@ -11,7 +10,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		return (0);
+		return (1);
 	}
 	else if (n < 0)
 	{
