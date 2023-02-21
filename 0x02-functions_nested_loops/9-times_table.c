cf1cd0d3032a3c454722ec822fec288b1e91b718
houcine <houcine.bh@pm.me>
2023-02-21 14:32:25 -0800

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 /**
  * times_table - prints the 9 times table, starting with 0.
  * Return: value of the last digit
@@ -13,7 +12,7 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			c = i * j;
-			write(1, &c, sizeof(c));
+			_putchar(c%10 + '0');
 		}
 		_putchar(10);
 
