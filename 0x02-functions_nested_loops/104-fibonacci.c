b46d8589068d28ae2e9f248a1a13d928d46705ce
houcine <houcine.bh@pm.me>
2023-02-21 17:42:24 -0800

    16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int a, s, i, j;
+
+	i = 0;
+	j = 1;
+	for (a = 1 ; a <= 98; a++)
+	{
+		s = i;
+		i = j;
+		j = i + s;
+		printf("%ld", j);
+		if (a != 98)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
