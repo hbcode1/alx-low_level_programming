688c2217f05baac9fa2a446a46021ae96b550d2c
houcine <houcine.bh@pm.me>
2023-02-22 05:02:48 -0800

    16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int a;
 
-	long double i, j, s;
+	unsigned long int i, j, s;
 
 	i = 0;
 	j = 1;
@@ -19,7 +19,7 @@ int main(void)
 		s = i;
 		i = j;
 		j = i + s;
-		printf("%Ld", j);
+		printf("%lu", j);
 		if (a != 98)
 			printf(", ");
 	}
