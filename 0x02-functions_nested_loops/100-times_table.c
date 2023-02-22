b36d1574acd9d95e97d193d65664ae7d8e5d80e1
houcine <houcine.bh@pm.me>
2023-02-21 16:11:33 -0800

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,9 @@ void print_times_table(int n)
 	int i, j, c;
 	
 	if (n < 15 && n >= 0)
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			c = i * j;
 			if (j > 0)
