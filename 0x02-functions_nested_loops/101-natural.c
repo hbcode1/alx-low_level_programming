a774c8b98424ec060942f5258a3982c295ec65aa
houcine <houcine.bh@pm.me>
2023-02-21 16:31:55 -0800

    13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_1024 - computes and prints the sum
+ * of all the multiples of 3 or 5 below 1024 (excluded),
+ * followed by a new line.
+ *
+ * Return: void
+ */
+void print_1024(void)
+{
+	int i;
+
+	for (i = 1 ; i < 1024; i++)
+	{
+		if (!(i % 3) || !(i % 5))
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+}
