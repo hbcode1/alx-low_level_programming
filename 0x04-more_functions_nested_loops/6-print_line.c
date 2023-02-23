b1ad04c01c056fe784daf51390e2c646330d17a2
houcine <houcine.bh@pm.me>
2023-02-23 07:56:21 -0800

    6. print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * followed by a new line.
+ * @n: length of the line
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar(10);
+}
