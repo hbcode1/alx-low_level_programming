8232d190907c03940ee69f38da093caf7874c084
houcine <houcine.bh@pm.me>
2023-02-23 15:02:54 -0800

    11. The largest prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,14 +2,14 @@
 
 /**
  * main - finds and prints the largest prime factor
- * of the number 612852475143, 
+ * of the number 612852475143,
  * followed by a new line.
  * isprime - check if number is prime
  * @x: to check if prime
  * Return: 0 success
  */
 
-int isprime(int x)
+int isprime(int x);
 
 int main(void)
 {
@@ -17,38 +17,37 @@ int main(void)
 	int temprime = 2;
 	int i;
 
-	for(i = 2; i <= number / 10000000; i++)
+	for (i = 2; i <= number / 10000000; i++)
 	{
 		if (isprime(i) && number % i == 0)
 		{
 			temprime = i;
-			while(number % i == 0)
+			while (number % i == 0)
 			{
 				number = number / temprime;
-				printf("%d %lu \n",i,number);
 			}
 		}
 	}
-	printf("%lu",number);
+	printf("%lu", number);
 	return (0);
 }
 
 int isprime(int x)
 {
-        int mult,i;
+	int mult, i;
 
-        mult = 0;
-        for(i = 2; i < x; i++)
-        {
-                if (!(x % i))
-                {
-                        mult = mult + 1;
-                }
-        }
-        if (mult > 0)
-                return (0);
-        else
-                return (1);
+	mult = 0;
+	for (i = 2; i < x; i++)
+	{
+		if (!(x % i))
+		{
+			mult = mult + 1;
+		}
+	}
+	if (mult > 0)
+		return (0);
+	else
+		return (1);
 
 }
 
