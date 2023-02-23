735df298a0df12929b0092091015427476ccaf00
houcine <houcine.bh@pm.me>
2023-02-23 08:46:08 -0800

    7. print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,11 +13,15 @@ void print_diagonal(int n)
 
 	for (i = 1; i <= n; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 1; j < i; j++)
 		{
 			_putchar(32);
 		}
 		_putchar(92);
+		_putchar(10);
+	}
+	if (i <= 0)
+	{
 		_putchar(10);
 	}
 }
