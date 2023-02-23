59eb91daf27a6615500125885a13b9f216469e8e
houcine <houcine.bh@pm.me>
2023-02-23 12:31:06 -0800

    10. draw triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle, followed by a new line.
+ * @size: dimension of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	/* cols */
+	for (i = 1; i <= size; i++)
+	{
+		/* rows */
+		/* spaces */
+		for (j = 1; j <= size - 1; j++)
+		{
+			_putchar(32);
+		}
+
+		/* hashs */
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(35);
+		}
+		_putchar(10);
+	}
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+}
