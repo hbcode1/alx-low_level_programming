1a4df1ebfcd61c3c48b4e11d60753538f53b6af4
houcine <houcine.bh@pm.me>
2023-02-28 12:36:35 -0800

    11. keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,5 +6,23 @@
 
 int main(void)
 {
+	int password[58];
+	int i, checksum = 0, extra;
+
+	srand(time(NULL));
+	for(i = 0; i < 58; i++)
+	{
+		password[i] = (rand() % ('~' - '0')) + '0';
+		checksum += password[i] + 48;
+		/* printf("%d\n",checksum);*/
+		putchar(password[i]);
+		if((checksum + 126) > 2772)
+		{
+			extra = 2772 - checksum;
+			/* printf("\n %d\n",checksum+extra);*/
+			putchar(extra);
+			break;
+		}
+	}
 	return (0);
 }
