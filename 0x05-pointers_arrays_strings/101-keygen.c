5f376b042ec7ceca4631781f4929d5891d477ce7
houcine <houcine.bh@pm.me>
2023-02-27 16:57:36 -0800

    add main

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
- *  reset_to_98 - Takes a pointer to an int as parameter
- *  and updates the value it points to to 98.
- *  @n: number to update
+ *  main - generates random valid passwords for the program 101-crackme. 
  *  Return: void
  */
 
-void reset_to_98(int *n)
+int main(void)
 {
-	*n = 98;
+	return (0);
 }
