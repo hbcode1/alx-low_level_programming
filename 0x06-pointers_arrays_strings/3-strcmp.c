11958e325a503d6abaa23cafa02b2641ce3210b8
houcine <houcine.bh@pm.me>
2023-03-02 07:40:37 -0800

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,15 +15,19 @@ int _strcmp(char *s1, char *s2)
 	int diff, i;
 	
 	i = 0;
-	while (*(s1 + i) != '\0' && *(s2 + i) ! = '\0')
+	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
 	{
 		if ( *(s1 + i) != *(s2 + i))
+		{
 			diff = *(s1 + i) - *(s2 + i);
 			break;
+		}
 		else
+		{
 			diff = *(s1 + i) - *(s2 + i);
+		}
 		i++;
 	}
-	return (i);
+	return (diff);
 }
 
