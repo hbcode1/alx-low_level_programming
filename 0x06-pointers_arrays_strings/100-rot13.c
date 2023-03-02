fa00ab246bd21524eb1608fd3e09376b44ca9991
houcine <houcine.bh@pm.me>
2023-03-02 12:29:44 -0800

    upload 8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rot13 - encodes a string using caesar cipher ROT13 encryption
+ * @str: string to encode.
+ * Return: encoded string.
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j = 0;
+	char letters[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
+
+	while (str[j] != '\0')
+	{
+		while (str[j] != letters[i] && letters[i] != '\0')
+		{
+			i++;
+		}
+		if (str[j] == letters[i])
+		{
+			str[j] = rot13[i];
+		}
+		i = 0;
+		j++;
+	}
+	return (str);
+}
