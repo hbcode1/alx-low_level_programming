a5f02f8712a5df7f5513f034bbf253ee5c665607
houcine <houcine.bh@pm.me>
2023-03-02 07:33:21 -0800

    correct ouptut

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	i = *s1 - *s2;
+	int diff, i;
+	
+	i = 0;
+	while (*(s1 + i) != '\0' && *(s2 + i) ! = '\0')
+	{
+		if ( *(s1 + i) != *(s2 + i))
+			diff = *(s1 + i) - *(s2 + i);
+			break;
+		else
+			diff = *(s1 + i) - *(s2 + i);
+	i++;
 	return (i);
 }
 
