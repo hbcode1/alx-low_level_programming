bac2d086d26d77c8b5352a13358b354960170ca2
houcine <houcine.bh@pm.me>
2023-03-02 11:23:44 -0800

    rewrite

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,17 +10,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len_dest, len_src, i, j = 0;
+	int len_dest, len_src, i;
 
 	for (len_dest = 0; *(dest + len_dest) != '\0'; len_dest++)
 		;
 	for (len_src = 0; *(src + len_src) != '\0'; len_src++)
 		;
-	for (i = len_dest; i < n && j < len_src; i++)
+	for (i = 0; i < n && i < len_src; i++)
 	{
-		*(dest + i) = *(src + j);
-		j++;
+		*(dest + len_dest + i) = *(src + i);
 	}
-		*(dest + i) = '\0';
+	*(dest + len_dest + i) = '\0';
 	return (dest);
 }
