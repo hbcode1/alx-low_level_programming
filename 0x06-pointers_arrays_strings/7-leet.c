5937a62751ab0c3c7415eee80400c769594564e8
houcine <houcine.bh@pm.me>
2023-03-02 12:16:50 -0800

    7. mozart

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * leet - encodes a string into 1337.
+ * @str: string to encode.
+ * Return: encoded string.
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j = 0;
+	char lowers[] = "aeotlAEOTL";
+	char mozart[] = "4307143071;
+
+	while (str[j] != '\0')
+	{
+		while (str[j] != lowers[i] && lowers[i] != '\0')
+		{
+			i++;
+		}
+		if (str[j] == lowers[i])
+		{
+			str[j] = mozart[i];
+		}
+		i = 0;
+		j++;
+	}
+	return (str);
+}
