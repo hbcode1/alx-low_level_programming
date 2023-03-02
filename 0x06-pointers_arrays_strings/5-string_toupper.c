60811d39a60d10506389336ea5ccd1e5446b0f42
houcine <houcine.bh@pm.me>
2023-03-02 07:53:54 -0800

    betty again

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * string_toupper - changes all lowercase letters 
+ * string_toupper - changes all lowercase letters
  * of a string to uppercase.
  * @str: string to convert.
- * Return: string uppercase. 
+ * Return: string uppercase.
  */
 
 char *string_toupper(char *str)
 {
-	int i = 0,j = 0;
+	int i = 0, j = 0;
 	char lowers[] = "abcdefghijklmnopqrstuvwxyz";
 	char uppers[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -17,7 +17,7 @@ char *string_toupper(char *str)
 	{
 		if (str[j] >= 97 && str[j] <= 122)
 		{
-			while ( str[j] != lowers[i])
+			while (str[j] != lowers[i])
 			{
 				i++;
 			}
@@ -26,10 +26,7 @@ char *string_toupper(char *str)
 		}
 		j++;
 	}
-	/* for(i = 65; i <= 90; i++)
-	 *{printf("%c",i);}
-      */
-     /* for(i = 97; i <= 122; i++){printf("%c",i);}
-      */
-	return str;
+	/* for(i = 65; i <= 90; i++){printf("%c",i);}*/
+     /* for(i = 97; i <= 122; i++){printf("%c",i);}*/
+	return (str);
 }
