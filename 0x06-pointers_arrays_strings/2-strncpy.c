d44ad11af50a0237d21c7f8c9a2073c62fb15055
houcine <houcine.bh@pm.me>
2023-03-02 07:16:52 -0800

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
 /**
  * _strncpy - copies a string.
  * @src: source string
- * @dest: destination string,
+ * @dest: destination string
  * @n: bytes
  * Return: pointer to the resulting string dest.
  */
