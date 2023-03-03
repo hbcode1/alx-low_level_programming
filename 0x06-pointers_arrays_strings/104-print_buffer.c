e6924e4d717ef28a5dca683d58853772459c7b1e
houcine <houcine.bh@pm.me>
2023-03-03 14:17:33 -0800

    upload 12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_buffer - prints a buffer.
+ * @b: the buffer
+ * @size: size of b
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	printf("buffer %s, size: %d", b, size);
+}
