e6313d4f564dfd7b02938bab0a6736b57df462d1
houcine <houcine.bh@pm.me>
2023-03-03 14:02:07 -0800

    betty

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * add - add two integers
+ * @a: first number.
+ * @b: second number.
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int x;
+	int sum = 0;
+
+	sum = a + b + x;
+	x = sum / 10;
+	return (sum % 10);
+}
+/**
  * infinit_add - adds two numbers.
  * @n1: first number.
  * @n2: second number.
  * @r: the buffer that the function zwill use to store the result
  * @size_r: the buffer size
  * Return: pointer to the result
- * 		0 if the result can not be stored in r
+ * 0 if the result can not be stored in r
  */
-
-int x = 0;
-
-int add(int a, int b)
-{
-	int sum = 0;
-	sum = a + b + x;
-	x = sum / 10;
-	return (sum % 10);
-}
-
 char *infinit_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i = 0, j = 1;
@@ -39,7 +44,7 @@ char *infinit_add(char *n1, char *n2, char *r, int size_r)
 		size_n2++;
 	}
 	i = 0;
-	if(size_n1 >= size_n2)
+	if (size_n1 >= size_n2)
 	{
 		j++;
 		r[size_r - 1] = 4;
