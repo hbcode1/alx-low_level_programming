cf2c410ff5f8423dfe0944726825c27dca8d33c5
houcine <houcine.bh@pm.me>
2023-03-06 13:59:30 -0800

    fix null

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,14 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != c && *s != '\0')
+	while (*s != '\0')
 	{
+		if (*s == c)
+		{
+			return (s);
+		}
 		s++;
 	}
-	if (*s == '\0')
-		return (0);
-	return (s);
+	return (0);
 }
 
