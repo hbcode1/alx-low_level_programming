b4b69189bfdca07faf10f52fa43373c65a093880
houcine <houcine.bh@pm.me>
2023-03-06 15:05:04 -0800

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
-* getlength - find string length.
-* @a: string
-* Return: string length.
-*/
-int getlength(char *a)
-{
-	int i, len;
-
-	while (*(a + i) != '\0')
-	{
-		len++;
-		i++;
-	}
-	return (len);
-}
-
-/**
 * _strstr - locates a substring.
 * finds the first occurrence of
 * @needle: the substring
@@ -27,46 +10,23 @@ int getlength(char *a)
 * - pointer to the beginning of the located substring
 * - NULL if the substring is not found.
 */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, temp_len = 0, reset;
-	int len = getlength(needle);
-	/* reset i */
-	i = 0;
-	/* itirate string*/
+	char *nee;
+	char *hay;
+
 	while (*haystack != '\0')
 	{
-		/* while no first match eleminate */
-		while (*haystack != *(needle + i))
+		nee = needle;
+		hay = haystack;
+		while (*haystack == *nee && *nee != '\0')
 		{
 			haystack++;
+			nee++;
 		}
-		/* from the first match calculate matches */
-		while (*haystack == *(needle + i) && *(needle + i) != '\0')
-		{
-			haystack++, temp_len++, i++;
-		}
-		/* when matches done compare to substirng length */
-		if (temp_len == len)
-		{
-			/* reset from the fisrt match */
-			for (reset = 0; reset < temp_len; reset++)
-			{
-				haystack--;
-			}
-			return (haystack);
-		}
-		else
-		{
-			/* reset from the character after the first match */
-			for (reset = 0; reset < temp_len - 1; reset++)
-			{
-				haystack--;
-			}
-			/* reset substring and match len */
-			temp_len = 0, i = 0;
-		}
+		if (*nee == '\0')
+			return (hay);
+		haystack++;
 	}
 	return ('\0');
 }
