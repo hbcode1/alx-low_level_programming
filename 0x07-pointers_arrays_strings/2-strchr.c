3e6977ae70faf2021e7ac82ecdb1f93021dfd99c
houcine <houcine.bh@pm.me>
2023-03-06 03:35:16 -0800

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string.
+* @s: string.
+* @c: character.
+* Return:
+* - pointer to the first occurrence of the character c in the string s.
+* - NULL if the character is not found.
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c && *s != '\0')
+	{
+		s++;
+	}
+	return (s);
+}
+
