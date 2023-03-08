6744c3a3f5efeec9616879a525e07b4f9cb27ca9
houcine <houcine.bh@pm.me>
2023-03-08 01:20:20 -0800

    betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
 /**
- * Return: factorial
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number
+ * Return: 
+ * if natural square root.
+ * else -1
  */
+int _sqrt_recursion(int n)
+{
+	
+}
