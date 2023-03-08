b97c71eea77f5696c4d877f6c289616615080d8e
houcine <houcine.bh@pm.me>
2023-03-07 23:27:08 -0800

    include main.h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: string.
  */
 
-void _puts_recursion(char *s)
+void _puts_recursion(char* s)
 {
 	if(*s != '\0')
 	_putchar(*s);
