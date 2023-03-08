bf75e89b9619542f2ab9fcf59a74245f1b041245
houcine <houcine.bh@pm.me>
2023-03-08 00:17:55 -0800

    2 strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s != '\0')
+		i++;
+	return (i);
+}
