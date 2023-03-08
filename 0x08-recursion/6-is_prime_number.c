9e6b6a2b49586cde519d2fa6abfaff8f25f0fe84
houcine <houcine.bh@pm.me>
2023-03-08 13:56:10 -0800

    6. isprime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,25 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_prime - check if number is prime using recursion.
+ * @n: number to check.
+ * @next: n - 1 to check if it s a factor of n
+ * Return:
+ * 1 if prime.
+ * 0 otherwise.
+ */
+
+int is_prime(int n, int next)
+{
+	/* case 1 is factor of all numbers */
+	if (next == 1)
+		return (1);
+	if (n % next == 0)
+		return (0);
+	next--;
+	return (is_prime(n, next));
+
+}
 
 /**
 * is_prime_number - check if number is prime using recursion
@@ -7,9 +28,13 @@
 */
 int is_prime_number(int n)
 {
-	if (n == 1)
+	/* 1 neither is a prime or composite */
+	/* primes are posirive */
+	if (n <= 1)
 		return (1);
-	if (n < 1)
-		return (0);
-	return (1 - is_prime_number(n - 1));
+	/* The perfect prime */
+	if (n == 2)
+		return (1);
+	/* call recursive function */
+	return (is_prime(n, n - 1));
 }
