c6db79de9a00e1e682f62e2762c140d2d73c30b5
houcine <houcine.bh@pm.me>
2023-03-08 02:25:23 -0800

    6 is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* is_prime_number - check if number is prime using recursion
+* @n: number to check
+* Return: 1 True, 0 False
+*/
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (1);
+	if (n < 1)
+		return (0);
+	return (1 - is_prime_number(n - 1));
+}
