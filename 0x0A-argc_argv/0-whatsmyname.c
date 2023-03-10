ad10caa54f831ad7168c3df2baba679678ee330b
houcine <houcine.bh@pm.me>
2023-03-10 06:28:25 -0800

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument count: number of arguments passed to the program.
+ * @argv: argument vector: one-dimensional array of arguments
+ * that was passed to the program.
+ * Return: 0 success
+ */
+
 int main(int argc, char **argv)
 {
-	argc =argc;
-	printf("%s\n",argv[0]);
-	return 0;
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
