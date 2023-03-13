24ac9aacdacdff44216dde8295d47d698ca14336
houcine <houcine.bh@pm.me>
2023-03-13 04:11:55 -0700

    size is 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,8 @@ char *create_array(unsigned int size, char c)
 {
 	char *a;
 	unsigned int i = 0;
-
+	if (size == 0)
+		return (0);
 	a = malloc(sizeof(char) * (size + 1));
 	if (a == NULL)
 		return (NULL);
