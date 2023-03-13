a335a7ff0f2e3527ba7304b6fe69cf3fd9d048ca
houcine <houcine.bh@pm.me>
2023-03-13 15:43:16 -0700

    3. alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,43 +3,33 @@
 
 /**
  * alloc_grid - returns a pointer to a 2D array of integers
- * @width: number of rows of the grid.
- * @height: number of columns of the grid.
+ * @w: number of rows of the grid.
+ * @h: number of columns of the grid.
  * Return:
  * - pointer to 2d array (Success).
  * - NULL (Failure).
  */
-int **alloc_grid(int width, int height)
+int **alloc_grid(int w, int h)
 {
-	int **a, i, j;
-	/* case w or h <= 0 */
-	if (width * height <= 0)
-		return (NULL);
-	/* allocate pointer */
-	a = malloc(sizeof(*a) * width);
-	if (a == NULL)
-	{
-		return (NULL);
-	}
-	/* allocate arrays */
-	for (i = 0; i < width; i++)
-	{
-		a[i] = malloc(sizeof(int) * height);
-		/* if failed free and retrun null*/
-		if (a[i] == NULL)
-		{
-			for (--i; i >= 0; i--)
-				free(a[i]);
-			free(a);
-			return (NULL);
-		}
-	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			a[i][j] = 0;
-		}
-	}
-	return (a);
+int **a, i, j;
+
+if (w * h <= 0)
+return (NULL);
+a = malloc(sizeof(int *) * h);
+for (i = 0; i < w; i++)
+{
+a[i] = malloc(sizeof(int) * w);
+if (a[i] == NULL)
+{
+for (--i; i >= 0; i--)
+free(a[i]);
+free(a);
+return (NULL);
 }
+}
+for (i = 0; i < h; i++)
+for (j = 0; j < w; j++)
+a[i][j] = 0;
+return (a);
+}
+
