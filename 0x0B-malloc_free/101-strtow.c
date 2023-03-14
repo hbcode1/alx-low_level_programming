ab3d75ac919c7f9cdf004ae4396cb3aa6cca2768
houcine <houcine.bh@pm.me>
2023-03-14 15:24:05 -0700

    strcmp

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -38,7 +38,7 @@ char **strtow(char *str)
 char **a;
 int count;
 
-if (str == NULL || strcmp(str,""))
+if (str == NULL || !strcmp(str, ""))
 	return (NULL);
 /* count words plus extra*/
 count = count_words(str);
