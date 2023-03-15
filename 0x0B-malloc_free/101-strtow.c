4812fc87e2c2183726652ba43e4a51054bda51cb
houcine <houcine.bh@pm.me>
2023-03-15 13:38:25 -0700

    upload local version

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 /**
 * count_words - counts words in string separated by spaces.
 * @str: string.
@@ -8,22 +9,22 @@
 */
 int count_words(char *str)
 {
-	int i, count = 0;
+int i, count = 0;
 
-	while (*(str + i) != '\0')
-	{
-		if (*(str + i) == ' ')
-			i++;
-		else
-		{
-			while (*(str + i) != ' ' && *(str + i) != '\0')
-			{
-				i++;
-			}
-			count++;
-		}
-	}
-	return (count);
+while (*(str + i) != '\0')
+{
+if (*(str + i) == ' ')
+i++;
+else
+{
+while (*(str + i) != ' ' && *(str + i) != '\0')
+{
+i++;
+}
+count++;
+}
+}
+return (count);
 }
 /**
 * strtow - splits a string into words.
@@ -38,14 +39,15 @@ char **a;
 int count;
 
 if (str == NULL || *str == '\0')
-	return (NULL);
+return (NULL);
 /* count words plus extra*/
 count = count_words(str);
+printf("count: %d", count);
 if (count == 0)
-	return (NULL);
+return (NULL);
 a = malloc(sizeof(char **) * count);
 if (a == NULL)
-	return (NULL);
+return (NULL);
 a[count - 1] = NULL;
 return (a);
 }
