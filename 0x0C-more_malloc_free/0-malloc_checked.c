816bf68bb52815f8dcf70c42e1890f5ce67c64f0
houcine <houcine.bh@pm.me>
2023-03-16 08:15:27 -0700

    new projects

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: size bytes.
+ * Return:
+ * - 98 if malloc fails.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	if (malloc(b) == NULL)
+		exit(0);
+	return (malloc(b));
+}
