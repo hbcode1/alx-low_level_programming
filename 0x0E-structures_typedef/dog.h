bf03ba9b9b1dab7d947bb60d600fa76063dc498f
houcine <houcine.bh@pm.me>
2023-03-20 05:18:47 -0700

    0. define new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define struct dog new type
+ *
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* end ifndef DOG_H */
