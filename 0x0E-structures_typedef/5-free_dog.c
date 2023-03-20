0be0d967f855068a91db960e22bcb34b39ee9589
houcine <houcine.bh@pm.me>
2023-03-20 07:57:02 -0700

    5. free struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - thi function that frees dogs allocated by malloc.
+ * @d: pointer to dog.
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
