a9d51f60f631ff7693122a320c59104dffcdcba8
houcine <houcine.bh@pm.me>
2023-03-20 13:53:55 -0700

    handle malloc return

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,10 +23,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	/* allocate memory for d_name string */
 	(*d).name = malloc(strlen(name) + 1);
+	if ((*d).name == NULL)
+		return (NULL);
 	/* store a copy of name in d_name */
 	d_name = strcpy((*d).name, name);
 	/* allocate memory for d_owner string */
 	(*d).owner = malloc(strlen(owner + 1));
+	if ((*d).owner == NULL)
+		return (NULL);
 	/* store a copy of owner in d_owner */
 	d_owner = strcpy((*d).owner, owner);
 	/* Create new dog */
