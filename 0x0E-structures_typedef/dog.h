f0455d4c5652a50b7e4ac4bcb46d78a059dda3c8
houcine <houcine.bh@pm.me>
2023-03-20 05:50:31 -0700

    1. initiale a variable struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - Define struct dog new type
  *
@@ -14,5 +13,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* end ifndef DOG_H */
