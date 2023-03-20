30f0f6735904607343152f5fbd26130ddbd00796
houcine <houcine.bh@pm.me>
2023-03-20 13:50:36 -0700

    use strcpy to store old values

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,13 +13,25 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *duck;
+	dog_t *d;
+	char *d_name, *d_owner;
 
-	duck = malloc(sizeof(dog_t));
-	if (duck == NULL)
-		return (duck);
-	memcpy(duck,char *name,sizeof(name));
-	memcpy(duck,char *owner,sizeof(owner));
-	duck->age = age;
-	return (duck);
+	/* allocate memory for d */
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (d);
+
+	/* allocate memory for d_name string */
+	(*d).name = malloc(strlen(name) + 1);
+	/* store a copy of name in d_name */
+	d_name = strcpy((*d).name, name);
+	/* allocate memory for d_owner string */
+	(*d).owner = malloc(strlen(owner + 1));
+	/* store a copy of owner in d_owner */
+	d_owner = strcpy((*d).owner, owner);
+	/* Create new dog */
+	d->name = d_name;
+	d->age = age;
+	d->owner = d_owner;
+	return (d);
 }
