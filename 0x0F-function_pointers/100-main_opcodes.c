700db8ab376df6f25a9d33f4882e0891be81afff
houcine <houcine.bh@pm.me>
2023-03-22 16:21:49 -0700

    print opcode of main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * main - prints the opcodes of its own.
+ *
+ * @ac: number of arguments passed to the program.
+ * @av: array of arguments passed to the program.
+ *
+ * Return: 0 Success.
+ */
+
+int main(int ac, char *av[])
+{
+	int (*ptr_main)() = &main, i = 0; /* pointer to main function */
+	u_c *opcode = (u_c *)ptr_main;
+
+	/* set 2 arguments rule */
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(av[1]) < 0)
+	{
+		printf("Error2\n");
+		exit(2);
+	}
+	while (i < atoi(av[1]))
+	{
+		printf("%02x ", *(opcode++));
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,4 +5,8 @@ int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+/**
+ * u_c - typedef for unsigned char
+ */
+typedef unsigned char u_c;
 #endif /* end def function_pointers.h */
