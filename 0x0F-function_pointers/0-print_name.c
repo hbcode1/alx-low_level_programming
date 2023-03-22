d07efc770a73ba22f515dea543031445235f1ae0
houcine <houcine.bh@pm.me>
2023-03-22 05:53:58 -0700

    create new dir with readme file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - This function prints a name.
+ *
+ * @name: name to print
+ * @f: pointer to function passed to print_name.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in an array
+ * and return it's index.
+ *
+ * @array: array.
+ * @size: size of array.
+ * @cmp: pointer to the function to be used to compare values.
+ *
+ * Return:
+ *	- The index of the first element.
+ *	- -1: if no element matches.
+ *	- -1: if size <= 0.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	/* case size <= 0 */
+	if (size <= 0)
+		return (-1);
+	/* itterate until first matche or the end */
+	while (cmp(array[i]) == 0 && i < size)
+		i++;
+	/* when cmp reaches the end with no match */
+	if (i == size)
+		return (-1);
+	return (i);
+}
