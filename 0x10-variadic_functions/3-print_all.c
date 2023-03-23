b042b09a5b4f4809177393caea1931d0682e9448
houcine <houcine.bh@pm.me>
2023-03-23 15:56:33 -0700

    restore last version

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,20 +5,6 @@
 #include <stdlib.h>
 
 /**
- * pstr - handle s format case.
- * @s: expected string.
- */
-
-void pstr(char *s)
-{
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", s);
-}
-/**
 * print_all - prints anything.
 *
 * @format: is a list of types of arguments passed to the function.
@@ -26,69 +12,40 @@ void pstr(char *s)
 
 void print_all(const char * const format, ...)
 {
-	va_list argp;
-	char *frm, *str;
-	int f = 0, i = 0, count = 0;
+va_list argp;
+char *frm;
 
-	/* allocate memory for temporary format */
-	frm = malloc(strlen(format) + 1);
-	/* store copy of format in temp */
-	strcpy(frm, format);
-	/* initialise pointer to last fixed arg */
-	va_start(argp, format);
+/* allocate memory for temporary format */
+frm = malloc(strlen(format) + 1);
+/* store copy of format in temp */
+strcpy(frm, format);
+/* initialise pointer to last fixed arg */
+va_start(argp, format);
 
-	/* get number of format expected */
-	while (*(frm + i) != '\0')
-	{
-		switch (*(frm + i))
-		{
-			case 'c':
-				count++;
-				break;
-			case 'i':
-				count++;
-				break;
-			case 'f':
-				count++;
-				break;
-			case 's':
-				count++;
-				break;
-			default:
-				i++;
-				continue;
-		}
-		i++;
-	}
-	while (*frm != '\0')
-	{
-		switch (*frm)
-		{
-			case 'c':
-				printf("%c", va_arg(argp, int));
-				f++;
-				break;
-			case 'i':
-				printf("%d", va_arg(argp, int));
-				f++;
-				break;
-			case 'f':
-				printf("%f", va_arg(argp, double));
-				f++;
-				break;
-			case 's':
-				str = va_arg(argp, char *);
-				f++;
-				pstr(str);
-				break;
-			default:
-				frm++;
-				continue;
-		}
-		if (*(frm + 1) != '\0' && count != f)
-			printf(", ");
-		frm++;
-	}
-	va_end(argp);
-	printf("\n");
+while (*frm != '\0')
+{
+switch (*frm)
+{
+case 'c':
+printf("%c", va_arg(argp, int));
+break;
+case 'i':
+printf("%d", va_arg(argp, int));
+break;
+case 'f':
+printf("%f", va_arg(argp, double));
+break;
+case 's':
+printf("%s", va_arg(argp, char *));
+break;
+default:
+frm++;
+continue;
+}
+if (*(frm + 1) != '\0')
+printf(", ");
+frm++;
+}
+va_end(argp);
+printf("\n");
 }
