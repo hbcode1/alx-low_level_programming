c5e3e4e2a7e07b435425659f8fcf285b93c7bcda
houcine <houcine.bh@pm.me>
2023-03-23 06:51:31 -0700

    using switch

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,32 +12,40 @@
 
 void print_all(const char * const format, ...)
 {
-va_list argp, argp1;
-char *frm;
+	va_list argp;
+	char *frm;
 
-/* allocate memory for temporary format */
-frm = malloc(strlen(format) + 1);
-/* store copy of format in temp */
-strcpy(frm, format);
-/* initialise pointer to last fixed arg */
-va_start(argp, format);
+	/* allocate memory for temporary format */
+	frm = malloc(strlen(format) + 1);
+	/* store copy of format in temp */
+	strcpy(frm, format);
+	/* initialise pointer to last fixed arg */
+	va_start(argp, format);
 
-va_copy(argp1, argp);
-while (*frm != '\0')
-{
-if (*frm == 'c')
-{
-printf("%c, ", va_arg(argp1, int));
-frm++;
+	while (*frm != '\0')
+	{
+		switch (*frm)
+		{
+			case 'c':
+				printf("%c", va_arg(argp, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(argp, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(argp, double));
+				break;
+			case 's':
+				printf("%s", va_arg(argp, char *));
+				break;
+			default:
+				frm++;
+				continue;
+		}
+		if (*(frm + 1) != '\0')
+			printf(", ");
+		frm++;
+	}
+	va_end(argp);
+	printf("\n");
 }
-if (*frm == 'i')
-{
-printf("%d, ", va_arg(argp1, int));
-frm++;
-}
-frm++;
-}
-va_end(argp);
-printf("\n");
-}
-
