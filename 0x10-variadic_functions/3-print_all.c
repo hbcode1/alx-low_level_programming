b2953de4e0d736003010f82ad119b1ad081c4970
houcine <houcine.bh@pm.me>
2023-03-23 15:51:08 -0700

    fix correct output

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -27,8 +27,8 @@ void pstr(char *s)
 void print_all(const char * const format, ...)
 {
 	va_list argp;
-	char *frm;
-	char *str;
+	char *frm, *str;
+	int f = 0, i = 0, count = 0;
 
 	/* allocate memory for temporary format */
 	frm = malloc(strlen(format) + 1);
@@ -37,28 +37,55 @@ void print_all(const char * const format, ...)
 	/* initialise pointer to last fixed arg */
 	va_start(argp, format);
 
+	/* get number of format expected */
+	while (*(frm + i) != '\0')
+	{
+		switch (*(frm + i))
+		{
+			case 'c':
+				count++;
+				break;
+			case 'i':
+				count++;
+				break;
+			case 'f':
+				count++;
+				break;
+			case 's':
+				count++;
+				break;
+			default:
+				i++;
+				continue;
+		}
+		i++;
+	}
 	while (*frm != '\0')
 	{
 		switch (*frm)
 		{
 			case 'c':
 				printf("%c", va_arg(argp, int));
+				f++;
 				break;
 			case 'i':
 				printf("%d", va_arg(argp, int));
+				f++;
 				break;
 			case 'f':
 				printf("%f", va_arg(argp, double));
+				f++;
 				break;
 			case 's':
 				str = va_arg(argp, char *);
+				f++;
 				pstr(str);
 				break;
 			default:
 				frm++;
 				continue;
 		}
-		if (*(frm + 1) != '\0')
+		if (*(frm + 1) != '\0' && count != f)
 			printf(", ");
 		frm++;
 	}
