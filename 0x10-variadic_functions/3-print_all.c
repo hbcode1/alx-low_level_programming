9884a712545837ddc4ecb3ba563068b8ffae8b36
houcine <houcine.bh@pm.me>
2023-03-23 16:00:12 -0700

    restore correct version

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,6 +5,20 @@
 #include <stdlib.h>
 
 /**
+ * pstr - handle s format case.
+ * @s: expected string.
+ */
+
+void pstr(char *s)
+{
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+/**
 * print_all - prints anything.
 *
 * @format: is a list of types of arguments passed to the function.
@@ -12,40 +26,42 @@
 
 void print_all(const char * const format, ...)
 {
-va_list argp;
-char *frm;
+	va_list argp;
+	char *frm;
+	char *str;
 
-/* allocate memory for temporary format */
-frm = malloc(strlen(format) + 1);
-/* store copy of format in temp */
-strcpy(frm, format);
-/* initialise pointer to last fixed arg */
-va_start(argp, format);
+	/* allocate memory for temporary format */
+	frm = malloc(strlen(format) + 1);
+	/* store copy of format in temp */
+	strcpy(frm, format);
+	/* initialise pointer to last fixed arg */
+	va_start(argp, format);
 
-while (*frm != '\0')
-{
-switch (*frm)
-{
-case 'c':
-printf("%c", va_arg(argp, int));
-break;
-case 'i':
-printf("%d", va_arg(argp, int));
-break;
-case 'f':
-printf("%f", va_arg(argp, double));
-break;
-case 's':
-printf("%s", va_arg(argp, char *));
-break;
-default:
-frm++;
-continue;
-}
-if (*(frm + 1) != '\0')
-printf(", ");
-frm++;
-}
-va_end(argp);
-printf("\n");
+	while (*frm != '\0')
+	{
+		switch (*frm)
+		{
+			case 'c':
+				printf("%c", va_arg(argp, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(argp, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(argp, double));
+				break;
+			case 's':
+				str = va_arg(argp, char *);
+				pstr(str);
+				break;
+			default:
+				frm++;
+				continue;
+		}
+		if (*(frm + 1) != '\0')
+			printf(", ");
+		frm++;
+	}
+	va_end(argp);
+	printf("\n");
 }
