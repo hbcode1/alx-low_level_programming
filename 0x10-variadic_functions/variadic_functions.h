5110117e5e6f834087bc922dce8292caac0f6efc
houcine <houcine.bh@pm.me>
2023-03-22 23:59:05 -0700

    upload header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+int _putchar(char);
+/**
+ * u_i : typedef of unsigned int
+ */
+typedef unsigned int u_i;
+#endif /* end def variadic_functions.h */
