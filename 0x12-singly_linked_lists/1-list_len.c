020caa1591f70416a077b255e27036f07e2386ad
hbcode1 <houcine.bh@pm.me>
2023-03-30 03:07:28 -0700

    fix betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * list_len - returns the number of elements in a linked list_t list.
- * 
+ *
  * @h: pointer to the fisrt element in list_t list.
  *
  * Return: Number f elements in a linked list.
@@ -10,6 +10,7 @@
 size_t list_len(const list_t *h)
 {
 	int count = 0;
+
 	while (h->next)
 	{
 		count++;
