5395510ab98fa97b1a99352d9ee16f98720c6f50
hbcode1 <houcine.bh@pm.me>
2023-03-30 15:35:43 -0700

    fix documentation

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,12 +6,13 @@
  * @head: pointer to the fisrt element in list_t list.
  */
 void free_list(list_t *head)
-{    
-     list_t *curr_node;
-     while ((curr_node = head) != NULL)
-     {    
-          head = head->next;
-          free(curr_node->str);
-          free(curr_node);
-     }    
-}  
+{
+	list_t *curr_node;
+
+	while ((curr_node = head) != NULL)
+	{
+		head = head->next;
+		free(curr_node->str);
+		free(curr_node);
+	}
+}
