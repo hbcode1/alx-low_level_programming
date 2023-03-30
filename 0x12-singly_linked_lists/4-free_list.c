45a9461b47151fc63b9b2b55b77e540c9159f7a1
hbcode1 <houcine.bh@pm.me>
2023-03-30 15:33:04 -0700

    fixing condition

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,10 +8,8 @@
 void free_list(list_t *head)
 {    
      list_t *curr_node;
-     curr_node = head;
-     while (curr_node != NULL)
+     while ((curr_node = head) != NULL)
      {    
-          curr_node = head;
           head = head->next;
           free(curr_node->str);
           free(curr_node);
