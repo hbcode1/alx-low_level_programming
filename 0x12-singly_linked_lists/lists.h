565b1f6e05bce64c074e856f7e9e4d3afb161e02
hbcode1 <houcine.bh@pm.me>
2023-03-30 03:10:17 -0700

    fix guard and add prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,4 +23,5 @@ int _putchar(char);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 #endif /* end def lists.h */
