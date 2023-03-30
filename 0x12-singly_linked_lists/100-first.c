da87e791dac6523cc77ecc35d4f4ff05202a9308
hbcode1 <houcine.bh@pm.me>
2023-03-30 16:01:53 -0700

    printf before main executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - prints You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n before the main function is executed.
+ */
+
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
