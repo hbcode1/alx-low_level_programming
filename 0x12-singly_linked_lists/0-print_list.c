1a9437801a1571ba85dbb2d5c777a5ee4197e29b
hbcode1 <houcine.bh@pm.me>
2023-03-30 03:06:30 -0700

    fix betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_list - prints all the elements of a list_t list.
- * 
+ *
  * @h: pointer to the fisrt element in list_t list.
  *
  * Return: Number of nodes
@@ -10,6 +10,7 @@
 size_t print_list(const list_t *h)
 {
 	int count = 0;
+
 	while (h->next)
 	{
 		printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
