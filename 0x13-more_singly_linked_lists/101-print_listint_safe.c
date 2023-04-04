30df1039940a883a4df7146de85d6161a7d0b0da
Houcine-code <houcine.bh@pm.me>
2023-04-04 08:47:02 +0000

    print list safely

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ *
+ * @h: Head node.
+ *
+ * Return:
+ *	- number of nodes in the list.
+ */
+
+size_t print_listint_safe(const listint_t *h)
+{
+	const listint_t *tmp, *match;
+	int count = 0, i = 0;
+
+	if (h)
+	{
+		/* store head */
+		tmp = h;
+		while (tmp->next)
+		{
+			/* reset match and macth index */
+			match = h, i = 0;
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			tmp = tmp->next;
+			count++;
+			while (i < count)
+			{
+				/* find first match */
+				if (match == tmp)
+				{
+					printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+					return (count);
+				}
+				match = match->next;
+				i++;
+			}
+		}
+	}
+	return (count);
+}
+
