717b816392a31fc327534f1d0b1ee239f479f740
Houcine-code <houcine.bh@pm.me>
2023-04-04 23:11:50 +0000

    restore working version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,7 +2,6 @@
 
 /**
  * print_listint_safe - prints a listint_t linked list.
- * 				using Floyd's cycle-finding algorithm.
  *
  * @h: Head node.
  *
@@ -12,59 +11,34 @@
 
 size_t print_listint_safe(const listint_t *h)
 {
-	const listint_t *target, *tmp1 = h, *tmp2 = h, *match;
-	int count = 0;
+	const listint_t *tmp, *match;
+	int count = 0, i = 0;
 
 	if (h)
 	{
-		/* detect loop */
-		while (tmp2 && tmp2->next)
+		/* store head */
+		tmp = h;
+		while (tmp->next)
 		{
-			tmp1 = tmp1->next;
-			tmp2 = tmp2->next->next;
-			if(tmp1 == tmp2)
-				break;
-		}
-		/* if no loop */
-		if (!tmp2 || !tmp2->next)
-		{
-			while ((target = h))
+			/* reset match and macth index */
+			match = h, i = 0;
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			tmp = tmp->next;
+			count++;
+			while (i < count)
 			{
-				printf("[%p] %d\n", (void *)target, target->n);
-				count++;
-				h = h->next;
+				/* find first match */
+				if (match == tmp)
+				{
+					printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+					return (count);
+				}
+				match = match->next;
+				i++;
 			}
-			return (count);
 		}
-		/* if loop */
-		/* break loop */
-		tmp1 = h;
-		while (tmp1 != tmp2)
-		{
-			tmp1 = tmp1->next;
-			tmp2 = tmp2->next;
-		}
-
-		match = tmp1;
-		target = h;
-		while (target != match)
-		{
-			printf("[%p] %d\n", (void *)target, target->n);
-			count++;
-			target = target->next;
-		}
-		printf("[%p] %d\n", (void *)target, target->n);
-		target = target->next;
-		while (target != match)
-		{
-			printf("[%p] %d\n",(void *)target, target->n);
-			count++;
-			target = target->next;
-		}
-		printf("-> [%p] %d\n", (void *)match, match->n);
-		if (h == NULL)
+		if (!h)
 			exit(98);
 	}
 	return (count);
 }
-
