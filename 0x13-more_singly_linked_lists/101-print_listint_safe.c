88fe5193d0142b6589a3c3afbb879c59dec4d1ee
Houcine-code <houcine.bh@pm.me>
2023-04-04 23:09:24 +0000

    clean junk

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -34,7 +34,6 @@ size_t print_listint_safe(const listint_t *h)
 				count++;
 				h = h->next;
 			}
-	printf("%d\n", count);
 			return (count);
 		}
 		/* if loop */
@@ -55,7 +54,6 @@ size_t print_listint_safe(const listint_t *h)
 			target = target->next;
 		}
 		printf("[%p] %d\n", (void *)target, target->n);
-		count++;
 		target = target->next;
 		while (target != match)
 		{
@@ -67,7 +65,6 @@ size_t print_listint_safe(const listint_t *h)
 		if (h == NULL)
 			exit(98);
 	}
-	printf("%d\n", count);
 	return (count);
 }
 
