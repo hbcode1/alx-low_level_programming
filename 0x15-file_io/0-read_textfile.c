e617e8b06067e8148c03fd63a9c4c444eae6e039
Houcine-code <houcine.bh@pm.me>
2023-04-07 16:20:14 +0000

    use read to check if not write expected amount

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,16 +17,19 @@
 ssize_t read_textfile(const char *name, size_t l)
 {
 	char *txt;
-	int in = open(name, O_RDONLY), count = 0;
+	int in = open(name, O_RDONLY), count = 0, len = 0;
 
 	txt = malloc(sizeof(char) * l);
 	if (txt == NULL)
-		return (STDIN_FILENO);
+		return (0);
 	if (in < 0 || !name)
-		return (STDIN_FILENO);
-	count = write(1, txt, read(in, txt, l));
-	if (count < 0)
-		return (STDIN_FILENO);
+		return (0);
+	len = read(in, txt, l);
+	if (len < 0)
+		return (0);
+	count = write(STDOUT_FILENO, txt, len);
+	if (count < 0 || len != count)
+		return (0);
 	close(in);
 	free(txt);
 	return (count);
