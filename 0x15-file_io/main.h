ba3f6dc8cfa5ced5973692ad3a518fc2ba9e3e1a
Houcine-code <houcine.bh@pm.me>
2023-04-07 16:49:30 +0000

    add Buffer size macro

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Macros */
+
+#define MAX_BUFF_SIZE 1024
+
+/* Prototypes */
+
 int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
