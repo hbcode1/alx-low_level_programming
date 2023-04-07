2e31e6f71b82ef1b776f565748719bbf954aaf9c
Houcine-code <houcine.bh@pm.me>
2023-04-07 13:57:02 +0000

    upload 1 2 tasks

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ *
+ * @name: name of the file to be created.
+ * @txt: content of the file (NULL terminated string)
+ *
+ * Return:
+ *   - 1 Success.
+ *   - (-1) Failure: 
+ *        - If file cant be created.
+ *        - If write fails.
+ *        - If filename is NULL.
+ *        - etc...
+ */
+
+int create_file(const char *name, char *txt)
+{
+	int in = open(name, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	int p;
+	if (!in || !name)
+		return (-1);
+	p = write(in, txt ? txt : "", txt ? strlen(txt) : 0);
+	if (p < 0)
+		return (p);
+	close(in);
+	return (1);
+}
+
