c4091c0fea803ec232096305e460035abdbb7458
Houcine-code <houcine.bh@pm.me>
2023-04-07 17:27:03 +0000

    adding error handler func

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -19,4 +19,5 @@ int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int print_error(int error, char *filename, int file_descriptor);
 #endif /* end def main.h */
