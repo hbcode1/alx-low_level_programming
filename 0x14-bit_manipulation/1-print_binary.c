fb6f5488e31ca7e32904ba665aae47d231845fd5
Houcine-code <houcine.bh@pm.me>
2023-04-08 14:57:08 +0000

    complete task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,4 +9,29 @@
 
 void print_binary(ul_i n)
 {
+	/* Define a display mask = 2147483648*/
+	u_i d = 1 << 31, l = 1, len = 0;
+	ul_i num = n;
+
+	if (n == 0)
+	{
+		_putchar(48);
+		return;
+	}
+	/* Get length of the number */
+	while (!(num & d) && len <= 32)
+		len++, num <<= 1;
+	/* shift display mask right */
+	while (len > 0)
+		d >>= 1, len--, l++;
+	/* bitwise OR number and display mask */
+	while (l <= 32)
+	{
+		if (n & d)
+			_putchar(49); /* if 1 print 1 */
+		else
+			_putchar(48); /* if 0 print 0 */
+		n <<= 1, l++; /* sheft left */
+	}
+	_putchar(10);
 }
