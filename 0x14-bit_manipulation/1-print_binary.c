a5fc849bb01e28bef56361ba35f8523cf8e926d1
Houcine-code <houcine.bh@pm.me>
2023-04-08 15:41:42 +0000

    fix shift overflow

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,27 +10,28 @@
 void print_binary(ul_i n)
 {
 	/* Define a display mask = 2147483648*/
-	u_i d = 1 << 63, l = 1, len = 0;
+	u_i d = 1 << 31, l = 1, len = 0;
 	ul_i num = n;
 
-	if (n == 0)
+	if (n)
 	{
-		_putchar(48);
-		return;
+		/* Get length of the number */
+		while (!(num & d) && len <= 32)
+			len++, num <<= 1;
+		/* shift display mask right */
+		while (len > 0)
+			d >>= 1, len--, l++;
+		/* bitwise OR number and display mask */
+		while (l <= 32)
+		{
+			if (n & d)
+				_putchar(49); /* if 1 print 1 */
+			else
+				_putchar(48); /* if 0 print 0 */
+			n <<= 1, l++; /* sheft left */
+			if (l % 8 == 0)
+				_putchar(' ');
+		}
 	}
-	/* Get length of the number */
-	while (!(num & d) && len <= 64)
-		len++, num <<= 1;
-	/* shift display mask right */
-	while (len > 0)
-		d >>= 1, len--, l++;
-	/* bitwise OR number and display mask */
-	while (l <= 64)
-	{
-		if (n & d)
-			_putchar(49); /* if 1 print 1 */
-		else
-			_putchar(48); /* if 0 print 0 */
-		n <<= 1, l++; /* sheft left */
-	}
+	_putchar('0');
 }
