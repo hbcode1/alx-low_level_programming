26c094f29b66ef4e38d29080d47e68abd380721a
Houcine-code <houcine.bh@pm.me>
2023-04-08 14:55:23 +0000

    clean code, add docs, reduce code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,14 +19,6 @@ u_i binary_to_uint(const char *b)
 
 	if (b)
 	{
-		/* eleminat first 0s */
-		while (*b != '\0')
-		{
-			if (*b == '0')
-				while (*b == '0')
-					b++;
-			break;
-		}
 		while (*(b + i) != '\0')
 		{
 			if (*(b + i) != '0')
@@ -34,9 +26,11 @@ u_i binary_to_uint(const char *b)
 				/* case unvalid character */
 				if (*(b + i) != '1')
 					return (0);
+				/* shift left and bitwise OR */
 				d = d << 1, d = d | 1;
 			}
 			else
+				/* shift left */
 				d = d << 1;
 			i++;
 		}
