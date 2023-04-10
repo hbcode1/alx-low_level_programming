12778f19e38716d0ec4b140f2df3bb69c7876764
Houcine-code <houcine.bh@pm.me>
2023-04-10 19:56:54 +0000

    trying betty

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,12 +1,46 @@
 #include "main.h"
 
 /**
+ * std_error - Entry point
+ * program that displays the information contained
+ * in the ELF header at the start of an ELF file.
+ *
+ * @id: array of argument passed to the program
+ *
+ * Return:
+ *	- 0 Success.
+ */
+
+int std_error(int id)
+{
+	switch (id)
+	{
+		case 0:
+			dprintf(1, "Usage: elf_filename\n");
+			exit(98);
+			break;
+		case 1:
+			dprintf(1, "open\n");
+			exit(98);
+			break;
+		case 2:
+			dprintf(1, "read\n");
+			exit(98);
+			break;
+		default:
+			dprintf(1, "close\n");
+			exit(98);
+			break;
+	}
+	return (0);
+}
+/**
  * main - Entry point
  * program that displays the information contained
  * in the ELF header at the start of an ELF file.
  *
  * @ac: number of arguments passed to the program
- * @ac: array of argument passed to the program
+ * @av: array of argument passed to the program
  *
  * Return:
  *	- 0 Success.
@@ -14,40 +48,32 @@
 
 int main(int ac, char *av[])
 {
-	int file, i;
-	Elf64_Ehdr head;
+	int file, i, c;
 
-	ac = ac;
+	Elf64_Ehdr head;
+	/* Usage */
+	if (ac != 2)
+		std_error(0);
 	/* open file */
 	file = open(av[1], O_RDONLY);
 	if (file < 0)
-	{
-		dprintf(1, "Can't open file: %s\n", av[1]);
-		exit(98);
-	}
+		std_error(1);
 	/* read file */
 	if (read(file, &head, sizeof(head)) < 0)
 	{
-		close(file);
-		if (file < 1)
-		{
-			dprintf(1, "Can't close file: %s\n", av[1]);
-			exit(98);
-		}
-		dprintf(1, "Can't read file: %s\n", av[1]);
-		exit(98);
+		c = close(file);
+		if (c < 1)
+			std_error(3);
+		std_error(2);
 	}
 	/* check if elf file */
-	if (!(head.e_ident[0] == 0x7f && head.e_ident[1] == 'E' && head.e_ident[2] == 'L' && head.e_ident[3] == 'F'))
+	if (!(head.e_ident[0] == 0x7f && head.e_ident[1] == 'E' &&
+				head.e_ident[2] == 'L' && head.e_ident[3] == 'F'))
 	{
 		close(file);
 		if (file < 1)
-		{
-			dprintf(1, "Can't close file: %s\n", av[1]);
-			exit(98);
-		}
-		dprintf(1, "Unvalid file format: %s\n", av[1]);
-		exit(98);
+			std_error(3);
+		std_error(2);
 	}
 	/* Print header information */
 	printf("ELF Header:\n");
@@ -55,20 +81,9 @@ int main(int ac, char *av[])
 	printf("Magic: ");
 	while (i < EI_NIDENT)
 		printf("%02x ", head.e_ident[i]), i++;
-	printf("\nClass: %s\n", (head.e_ident[EI_CLASS] == ELFCLASS32) ? "32" : "64");
-	printf("Data: %s\n", head.e_ident[EI_DATA] == ELFDATA2LSB ? "little" : " big");
-	printf("Version: %d\n", head.e_ident[EI_VERSION]);
-	printf("OS/ABI: %d\n", head.e_ident[EI_OSABI]);
-	printf("ABI Version: %d\n", head.e_ident[EI_ABIVERSION]);
-	printf("Type: %d\n", head.e_type);
-	printf("Entry point address: %#lx\n", head.e_entry & 0xffffff);
-
 	/* close file */
 	close(file);
 	if (file < 1)
-	{
-		dprintf(1, "Can't close file: %s\n", av[1]);
-		exit(98);
-	}
+		std_error(3);
 	return (0);
 }
