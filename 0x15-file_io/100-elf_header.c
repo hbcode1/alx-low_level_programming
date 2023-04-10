b6231297a9eb58c85ab058c3c4abc5ae8ddae286
Houcine-code <houcine.bh@pm.me>
2023-04-10 16:10:39 +0000

    add description

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ * program that displays the information contained
+ * in the ELF header at the start of an ELF file.
+ *
+ * @ac: number of arguments passed to the program
+ * @ac: array of argument passed to the program
+ *
+ * Return:
+ *	- 0 Success.
+ */
+
 int main(int ac, char *av[])
 {
 	FILE *file;
@@ -9,26 +21,26 @@ int main(int ac, char *av[])
 	/* Usage */
 	if (ac != 2)
 	{
-		printf("Usage: %s elf_filename", av[0]);
+		dprintf("Usage: %s elf_filename", av[0]);
 		exit(98);
 	}
 	/* open file */
 	file = fopen(av[1], "r");
 	if (!file)
 	{
-		printf("Can't open file: %s", av[1]);
+		dprintf("Can't open file: %s", av[1]);
           exit(98);
 	}
 	/* read file */
 	if (fread(&head, sizeof(head), 1, file) != 1)
 	{
-		printf("Can't read file: %s", av[1]);
+		dprintf("Can't read file: %s", av[1]);
           exit(98);
 	}
 	/* check if elf file */
 	if (!(head.e_ident[0] == 0x7f && head.e_ident[1] == 'E' && head.e_ident[2] == 'L' && head.e_ident[3] == 'F'))
 	{
-		printf("Unvalid file format: %s", av[1]);
+		dprintf("Unvalid file format: %s", av[1]);
           exit(98);
 	}
 	/* Print header information */
