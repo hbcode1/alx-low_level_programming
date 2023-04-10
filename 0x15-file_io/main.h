5d43514d38dd2feccabb59c7ceb7dbef609c4d02
Houcine-code <houcine.bh@pm.me>
2023-04-10 23:31:50 +0000

    add func for elf task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,4 +20,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int print_error(int error, char *filename, int file_descriptor);
+
+/* elf task */
+int pstderr(int id, char *data);
+int checkfile(unsigned char *m, int f, char *data);
+char *print_os_abi(int oa);
+char *otype(int t);
 #endif /* end def main.h */
