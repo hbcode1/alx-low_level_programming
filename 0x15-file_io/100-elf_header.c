07f2f27f39fb5bdfd6fb1872fba5ebf1c871e98a
Houcine-code <houcine.bh@pm.me>
2023-04-10 23:33:36 +0000

    adding functions, devlping code, handling format

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,130 @@
 #include "main.h"
 
 /**
+ * otype - return the object file type
+ *
+ * @t: object type id.
+ *
+ * Return:
+ *	- object file type
+ */
+char *otype(int t)
+{
+	switch (t)
+	{
+		case ET_REL:
+			return ("REL (Relocatable file)");
+		case ET_EXEC:
+			return ("EXEC (Executable file)");
+		case ET_DYN:
+			return ("DYN (shared object file)");
+		case ET_CORE:
+			return ("core file");
+		default:
+			return ("NONE (Unknown type)");
+	}
+}
+/**
+ * os_abi - return one of The eighth byte
+ * identifies the operating system and ABI to which  the  object  is targeted.
+ *
+ * @oa: EI_OSABI value to check.
+ *
+ * Return:
+ *	- OS and ABI
+ */
+char *os_abi(int oa)
+{
+	switch (oa)
+	{
+		case ELFOSABI_NONE || ELFOSABI_SYSV:
+			return ("UNIX");
+		case ELFOSABI_HPUX:
+			return ("UNIX - HP-UX");
+		case ELFOSABI_NETBSD:
+			return ("UNIX - NetBSD");
+		case ELFOSABI_LINUX:
+			return ("UNIX - Linux");
+		case ELFOSABI_SOLARIS:
+			return ("UNIX - Solaris");
+		case ELFOSABI_IRIX:
+			return ("UNIX - IRIX");
+		case ELFOSABI_FREEBSD:
+			return ("UNIX - FreeBSD");
+		case ELFOSABI_TRU64:
+			return ("UNIX - TRU64");
+		case ELFOSABI_ARM:
+			return ("UNIX - ARM");
+		case ELFOSABI_STANDALONE:
+			return ("UNIX - Stand-alone (embedded)");
+		default:
+			return ("unknown");
+	}
+}
+
+/**
+ * checkfile - check the type of file if it s elf.
+ *
+ * @m: magic number of the file.
+ * @f: file descriptor.
+ * @data: name of file.
+ *
+ * Return:
+ *	- 0 Success.
+ */
+int checkfile(unsigned char *m, int f, char *data)
+{
+	int c;
+
+	if (!(m[0] == 0x7f && m[1] == 'E' && m[2] == 'L' && m[3] == 'F'))
+	{
+		c = close(f);
+		if (c < 1)
+			pstderr(3, data);
+		pstderr(4, data);
+	}
+	return (0);
+}
+/**
+ * pstderr - handle error messages.
+ * send error message to stderr
+ *
+ * @id: id of the error.
+ * @data: string to print.
+ *
+ * Return:
+ *	- 0 Success
+ */
+
+int pstderr(int id, char *data)
+{
+	switch (id)
+	{
+		case 0:
+			dprintf(1, "Usage: %s elf_filename\n", data);
+			exit(98);
+			break;
+		case 1:
+			dprintf(1, "Can't open file: %s\n", data);
+			exit(98);
+			break;
+		case 2:
+			dprintf(1, "Can't read file: %s\n", data);
+			exit(98);
+			break;
+		case 3:
+			dprintf(1, "Can't close file: %s\n", data);
+			exit(98);
+			break;
+		default:
+			dprintf(1, "Unvalid file format: %s\n", data);
+			exit(98);
+			break;
+	}
+	return (0);
+}
+
+/**
  * main - Entry point
  * program that displays the information contained
  * in the ELF header at the start of an ELF file.
@@ -14,66 +138,45 @@
 
 int main(int ac, char *av[])
 {
-	int file, i, c;
-
+	int file, i, c, v;
 	Elf64_Ehdr head;
-	/* Usage */
+
 	if (ac != 2)
-	{
-		dprintf(1, "Usage: %s elf_filename\n", av[0]);
-		exit(98);
-	}
-	/* open file */
-	file = open(av[1], O_RDONLY);
+		pstderr(0, av[0]);/* Usage */
+	file = open(av[1], O_RDONLY);/* open file */
 	if (file < 0)
-	{
-		dprintf(1, "Can't open file: %s\n", av[1]);
-		exit(98);
-	}
-	/* read file */
+		pstderr(1, av[0]);
 	if (read(file, &head, sizeof(head)) < 0)
 	{
 		c = close(file);
 		if (c < 1)
-		{
-			dprintf(1, "Can't close file: %s\n", av[1]);
-			exit(98);
-		}
-		dprintf(1, "Can't read file: %s\n", av[1]);
-		exit(98);
+			pstderr(3, av[1]);
+		pstderr(2, av[1]);
 	}
 	/* check if elf file */
-	if (!(head.e_ident[0] == 0x7f && head.e_ident[1] == 'E' && head.e_ident[2] == 'L' && head.e_ident[3] == 'F'))
-	{
-		c = close(file);
-		if (c < 1)
-		{
-			dprintf(1, "Can't close file: %s\n", av[1]);
-			exit(98);
-		}
-		dprintf(1, "Unvalid file format: %s\n", av[1]);
-		exit(98);
-	}
-	/* Print header information */
+	checkfile(head.e_ident, file, av[1]);
 	printf("ELF Header:\n");
-	/* print magic number */
-	printf("Magic: ");
+	printf("  Magic:   ");/* print magic number */
 	while (i < EI_NIDENT)
 		printf("%02x ", head.e_ident[i]), i++;
-	printf("\nClass: %s\n", (head.e_ident[EI_CLASS] == ELFCLASS32) ? "32" : "64");
-	printf("Data: %s\n", head.e_ident[EI_DATA] == ELFDATA2LSB ? "little" : " big");
-	printf("Version: %d\n", head.e_ident[EI_VERSION]);
-	printf("OS/ABI: %d\n", head.e_ident[EI_OSABI]);
-	printf("ABI Version: %d\n", head.e_ident[EI_ABIVERSION]);
-	printf("Type: %d\n", head.e_type);
-	printf("Entry point address: %#lx\n", head.e_entry & 0xffffff);
+	printf("\n  Class:                             %s\n",
+			(head.e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("  Data:                              2's complement, %s endian\n",
+			head.e_ident[EI_DATA] == ELFDATA2LSB ? "little" : " big");
+	v = head.e_ident[EI_VERSION];
+	printf("  Version:                           %d%s\n",
+	v, v == EV_CURRENT ? " (current)" : "");
+	printf("  OS/ABI:                            %s\n",
+			os_abi(head.e_ident[EI_OSABI]));
+	printf("  ABI Version:                       %d\n",
+			head.e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %s\n", otype(head.e_type));
+	printf("  Entry point address:               %#lx\n",
+			head.e_entry & 0xfffffffff);
 
 	/* close file */
 	c = close(file);
 	if (c < 1)
-	{
-		dprintf(1, "Can't close file: %s\n", av[1]);
-		exit(98);
-	}
+		pstderr(3, av[1]);
 	return (0);
 }
